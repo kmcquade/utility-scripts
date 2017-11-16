47d3f59326cd1c47f6e4b174d48309f9772e14a3
Ohad David <Ohad.David@borderfree.com>
2017-11-16 11:49:05 +0200

    fixed parameters

--- watch-s3-file.sh
+++ watch-s3-file.sh
@@ -5,16 +5,16 @@ program=$0
 
 usage(){
     cat << EOF
-Usage: $program <params>
-Parameters:
-    --s3-url <s3-url>                               - S3 file to be watched and fetched
-    --file-name <destination-file>                  - Absolute destination filename
-    [--cache-dir <cache-dir>]                       - Cached directory (Default: /tmp/.s3.cache)
-    [--polling-period <polling-period-in-seconds>]  - Polling in seconds (Default 20)
-    [--command <command-to-execute-on-change>]      - Command to be executed if the file was changed
-    [--first-time-execute]                          - Should execute command on first time (Default: false)
-    [--skip-watch]                                  - Skip watch the file, only fetch it once (Default: false)
-    [--debug]                                       - Log debug (Default: false)
+Usage: $program [options...] --s3-url <s3-url> --file-name <destination-file>
+Options:
+-u, --s3-url <s3-url>                               - S3 file to be watched and fetched
+-f, --file-name <destination-file>                  - Absolute destination filename
+-d,--cache-dir <cache-dir>                          - Cached directory (Default: /tmp/.s3.cache)
+--polling-interval <polling-interval-in-seconds>]   - Polling interval in seconds (Default 30)
+--command <command-to-execute-on-change>            - Command to be executed if the file was changed
+--first-time-execute                                - Should execute command on first time (Default: false)
+--skip-watch                                        - Skip watch the file, only fetch it once (Default: false)
+--debug                                             - Log debug (Default: false)
     
 EOF
     exit 1
@@ -30,26 +30,26 @@ debug(){
 
 
 fetch_config(){
-    debug "Fetching ${S3_URL} to ${S3_SYNC_DIR}"
-    aws --region=${S3_BUCKET_LOCATION} s3 cp ${S3_URL} ${S3_SYNC_DIR}/${FILE_BASENAME} --quiet
-    debug "Calulating checksum of ${S3_SYNC_DIR}/${FILE_BASENAME}"
-    MD5_CHECKSUM=$(md5 -q ${S3_SYNC_DIR}/${FILE_BASENAME})
+    debug "Fetching ${S3_URL} to ${CACHE_DIR}"
+    aws --region=${S3_BUCKET_LOCATION} s3 cp ${S3_URL} ${CACHE_DIR}/${FILE_BASENAME} --quiet
+    debug "Calulating checksum of ${CACHE_DIR}/${FILE_BASENAME}"
+    MD5_CHECKSUM=$(md5 -q ${CACHE_DIR}/${FILE_BASENAME})
     debug "MD5_CHECKSUM = $MD5_CHECKSUM"
-    if [ -f ${S3_SYNC_DIR}/${FILE_BASENAME}.md5 ]; then
-        ORIG_MD5_CHECKSUM=$(cat ${S3_SYNC_DIR}/${FILE_BASENAME}.md5)
+    if [ -f ${CACHE_DIR}/${FILE_BASENAME}.md5 ]; then
+        ORIG_MD5_CHECKSUM=$(cat ${CACHE_DIR}/${FILE_BASENAME}.md5)
     else
         ORIG_MD5_CHECKSUM=''
     fi
     debug "ORIG_MD5_CHECKSUM = $ORIG_MD5_CHECKSUM"
-    md5 -q ${S3_SYNC_DIR}/${FILE_BASENAME} > ${S3_SYNC_DIR}/${FILE_BASENAME}.md5
+    md5 -q ${CACHE_DIR}/${FILE_BASENAME} > ${CACHE_DIR}/${FILE_BASENAME}.md5
     if [ "$ORIG_MD5_CHECKSUM" != "$MD5_CHECKSUM" ]; then
-        cp ${S3_SYNC_DIR}/${FILE_BASENAME} ${OUT_FILENAME}
+        cp ${CACHE_DIR}/${FILE_BASENAME} ${FILENAME}
         return 30
     fi
     return 0
 }
 
-update_config(){
+exec_command(){
     if [ -n "$COMMAND" ]; then
         log "Running $COMMAND."
         eval "$COMMAND"
@@ -71,19 +71,19 @@ parseArgs(){
         ;;
         
         -d|--cache-dir)
-        S3_SYNC_DIR="$2"
+        CACHE_DIR="$2"
         shift # past argument
         shift # past value
         ;;
         
-        -p|--polling-period)
-        S3_POLLING_PERIOD="$2"
+        -p|--polling-interval)
+        POLLING_INTERVAL="$2"
         shift # past argument
         shift # past value
         ;;
         
         -f|--filename)
-        OUT_FILENAME="$2"
+        FILENAME="$2"
         shift # past argument
         shift # past value
         ;;
@@ -96,6 +96,11 @@ parseArgs(){
 
         --skip-watch)
         SKIP_WATCH=YES
+        shift # past argument
+        ;;
+
+        --first-time-execute)
+        FIRST_TIME_EXECUTE=YES
         shift # past argument
         ;;
         
@@ -115,44 +120,49 @@ parseArgs(){
         (>&2 echo "Error: --s3-url must be provided")
         usage
     fi
-    if [[ -z "$OUT_FILENAME" ]]; then 
+    if [[ -z "$FILENAME" ]]; then 
         (>&2 echo "Error: --filename must be provided")
         usage
     fi
 
-    S3_POLLING_PERIOD=${S3_POLLING_PERIOD:-20}
-    S3_SYNC_DIR=${S3_SYNC_DIR:-/tmp/.s3.cache}
+    POLLING_INTERVAL=${POLLING_INTERVAL:-30}
+    CACHE_DIR=${CACHE_DIR:-/tmp/.s3.cache}
     S3_BUCKET=$(echo $S3_URL | sed 's/s3:\/\///g' | cut -d'/' -f 1)
     S3_BUCKET_LOCATION="$(aws s3api get-bucket-location --bucket ${S3_BUCKET} --output text)"
-    FILE_BASENAME=$(basename $OUT_FILENAME)
+    FILE_BASENAME=$(basename $FILENAME)
 }
 
 parseArgs "$@"
 log "$program started"
 log "S3_URL = $S3_URL"
-log "S3_SYNC_DIR = $S3_SYNC_DIR"
-log "S3_POLLING_PERIOD = $S3_POLLING_PERIOD"
-log "OUT_FILENAME = $OUT_FILENAME"
+log "CACHE_DIR = $CACHE_DIR"
+log "POLLING_INTERVAL = $POLLING_INTERVAL"
+log "FILENAME = $FILENAME"
 log "COMMAND = $COMMAND"
 log "SKIP_WATCH = $SKIP_WATCH"
 log "DEBUG = $DEBUG"
 
-mkdir -p $S3_SYNC_DIR
+mkdir -p $CACHE_DIR
 
-log "Fetching $OUT_FILENAME for the first time..."
+log "Fetching $FILENAME for the first time..."
 fetch_config && FETCH_RES=$? || FETCH_RES=$?
 
-if [ -n "$SKIP_WATCH" ]; then
+if [ "$FIRST_TIME_EXECUTE" == "YES" ]; then
+    log "FIRST_TIME_EXECUTE = YES, executing command"
+    exec_command
+fi
+
+if [ "$SKIP_WATCH" == "YES" ]; then
     log "SKIP_WATCH = YES, Going out..."
     exit 0
 fi
 
 log "Entering watch loop"
 while true; do
-    sleep $S3_POLLING_PERIOD
+    sleep $POLLING_INTERVAL
     fetch_config && RES=$? || RES=$?
     if [ "$RES" == 30 ]; then
         log "Checksum was changed, executing command"
-        update_config
+        exec_command
     fi
 done
